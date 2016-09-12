4ac49eeb866671e9dc4cc0818ad3bb95d66d0924
bdesilva <bdesilva@uab.edu>
2016-09-12 11:48:48 -0500

    Working Version of the extract script

--- extract_genomic_diff.py
+++ extract_genomic_diff.py
@@ -1,19 +1,74 @@
-# Input: the output of the get_paired_vcf.py file and a gff file
-# TO-DO: identify the differences and map to a gene
+#! /bin/python
 
-# read in the gff file to find in which gene the location of the SNP difference was
-# Record the gene and the location in the gene and the file with the SNP
-# Save this to a file with the species name, then allow the other t2 files to append their data to the table
+# Input: the output of the get_paired_vcf.py file
+# TO-DO: identify the differences and write to a new file 
 
-# Example file:
+import argparse
+import csv
+import sys
+import pdb
 
-## Bacteroides_vulgatus
-# t1	t2	gene	location (bp)
-# 1	0	3	46500
-# 0	1	18	180049
+def format_row(row):
+	return '%s' % '\t'.join(map(str, row)) + '\n'
 
-# t1	t3	gene	location (bp)
-# 1	0	1	2500
-# 0	1	3	4800
 
-# separate files for different t1 files
+# argparse
+parser = argparse.ArgumentParser(description="""Find the SNP differences between the two samples provided in the
+intermediate multi-sample vcf file. Only look for the combinations: 1 0 or 0 1 (where 1 indicates a SNP and 0 indicates
+no SNP).""")
+parser.add_argument('input',help="""Specify a multi-sample vcf file with 2 linked samples (ex: time separated from the
+same donor).""")
+parser.add_argument('-p', '--position',help="""If the samples are not the last 2 columns of the vcf file, indicate the
+column number (from left to right) of the first vcf sample (this will assume that the second vcf file is directly
+following the first). Or specify the first vcf sample column number and the second vcf sample column
+number.""",nargs='+')
+
+args = parser.parse_args()
+
+first_flag = False # first time through flag
+new_file = args.input + '.diff'
+zero_based = 1 # to ameliorate the zero-based and one-based counting of a user v. python
+
+# get the sample column position
+if args.position is not None:
+	if len(args.position) > 2:
+		sys.exit('Input only 2 sample positions.\n')
+	elif len(args.position) == 2:
+		p_one = int(args.position[0]) - zero_based
+		p_two = int(args.position[1]) - zero_based
+		first_flag = True
+	elif len(args.position) == 1:
+		p_one = int(args.position[0]) - zero_based
+		p_two = p_one + 1
+		first_flag = True
+
+count = 0
+# open file and write to new file
+with open(args.input) as f, open(new_file, 'w+') as out:
+	multi = csv.reader(f, delimiter="\t")
+	for row in multi:
+		# get the sample position
+		if not first_flag and args.position is None: # this way only executes check to true or false each iteration
+			p_two = len(row) - 1
+			p_one = p_two - 1
+			first_flag = True
+			out.write(format_row(row))
+			count = 1
+			continue # skip the header row
+		if count == 0:
+			out.write(format_row(row))
+			count = 1
+			continue
+		# check the two positions
+		# print the positions to file
+		if row[p_one][0] == '.' or row[p_two][0] == '.':
+			# if is any combination with '.' (meaning no information)
+			continue
+		elif int(row[p_one][0]) == 1 and int(row[p_two][0]) == 0:
+			out.write(format_row(row))
+		elif int(row[p_one][0]) == 0 and int(row[p_two][0]) == 1:
+			out.write(format_row(row))
+		else:
+			# either 1 and 1 or 0 and 0
+			continue
+# the final 2 columns are for comparison
