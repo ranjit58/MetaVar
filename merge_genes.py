ccc4881655c9e993507e412768fcaa06ddc715bb
bdesilva <bdesilva@uab.edu>
2016-09-19 16:33:02 -0500

    Update the merge script description

---
+++ merge_genes.py
@@ -0,0 +1,8 @@
+#/bin/python
+
+# Takes differenced pairwise samples and combines them into a table format
+# Does not need to preserve the bp changes, only an indication of change or no change
+# Table design: coordinate|annotation|1|2|3|4|Sum (samples 1, 2, 3, 4)
+
+
+
